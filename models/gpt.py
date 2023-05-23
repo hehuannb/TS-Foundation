c7e3c5aea1e0a3cc7bf3ef2ac6c4670020581f10
Huan He <huan_he@hms.harvard.edu>
2023-05-23 15:03:50 -0400

    add model

---
+++ models/gpt.py
@@ -0,0 +1,125 @@
+from typing import Optional
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import optim
+
+from transformers import GPT2ForSequenceClassification
+from transformers.models.gpt2.modeling_gpt2 import GPT2Model
+from transformers.models.gpt2.configuration_gpt2 import GPT2Config
+from transformers import BertTokenizer, BertModel
+from einops import rearrange
+from layers.Embed import DataEmbedding, DataEmbedding_wo_time
+
+
+class Model(nn.Module):
+    
+    def __init__(self, configs):
+        super(Model, self).__init__()
+        self.is_ln = configs.ln
+        self.task_name = configs.task_name
+        self.pred_len = configs.pred_len
+        self.seq_len = configs.seq_len
+        self.patch_size = configs.patch_size
+        self.stride = configs.stride
+        self.seq_len = configs.seq_len
+        self.d_ff = configs.d_ff
+        self.patch_num = (configs.seq_len + self.pred_len - self.patch_size) // self.stride + 1
+
+        self.padding_patch_layer = nn.ReplicationPad1d((0, self.stride)) 
+        self.patch_num += 1
+        self.enc_embedding = DataEmbedding(configs.enc_in * self.patch_size, configs.d_model, configs.embed, configs.freq,
+                                           configs.dropout)
+
+        self.gpt2 = GPT2Model.from_pretrained('gpt2', output_attentions=True, output_hidden_states=True)
+        self.gpt2.h = self.gpt2.h[:configs.gpt_layers]
+        
+        for i, (name, param) in enumerate(self.gpt2.named_parameters()):
+            if 'ln' in name or 'wpe' in name: # or 'mlp' in name:
+                param.requires_grad = True
+            elif 'mlp' in name and configs.mlp == 1:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+
+        if configs.use_gpu:
+            device = torch.device('cuda:{}'.format(0))
+            self.gpt2.to(device=device)
+
+        # self.in_layer = nn.Linear(configs.patch_size, configs.d_model)
+
+        if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
+            self.predict_linear_pre = nn.Linear(self.seq_len, self.pred_len + self.seq_len)
+            self.predict_linear = nn.Linear(self.patch_size, configs.enc_in)
+            self.ln = nn.LayerNorm(configs.d_ff)
+            self.out_layer = nn.Linear(configs.d_ff, configs.c_out)
+        if self.task_name == 'classification':
+            self.act = F.gelu
+            self.dropout = nn.Dropout(0.1)
+            self.ln_proj = nn.LayerNorm(configs.d_model * self.patch_num)
+            self.out_layer = nn.Linear(configs.d_model * self.patch_num, (self.pred_len + self.seq_len) * configs.c_out)
+
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask=None):
+        if self.task_name == 'long_term_forecast' or self.task_name == 'short_term_forecast':
+            dec_out = self.forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
+            return dec_out[:, -self.pred_len:, :]  # [B, L, D]
+        if self.task_name == 'classification':
+            dec_out = self.classification(x_enc, x_mark_enc)
+            return dec_out  # [B, N]
+        return None
+
+    def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
+        B, L, M = x_enc.shape
+        
+        # Normalization from Non-stationary Transformer
+        means = x_enc.mean(1, keepdim=True).detach()
+        x_enc = x_enc - means
+        stdev = torch.sqrt(
+            torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
+        x_enc /= stdev
+
+        # embedding
+        enc_out = self.enc_embedding(x_enc, x_mark_enc)  # [B,T,C]
+        enc_out = self.predict_linear_pre(enc_out.permute(0, 2, 1)).permute(
+            0, 2, 1)  # align temporal dimension
+        enc_out = torch.nn.functional.pad(enc_out, (0, 768-enc_out.shape[-1]))
+
+        dec_out = self.gpt2(inputs_embeds=enc_out).last_hidden_state
+        dec_out = dec_out[:, :, :self.d_ff]
+        # dec_out = dec_out.reshape(B, -1)
+        
+        # dec_out = self.ln(dec_out)
+        dec_out = self.out_layer(dec_out)
+        # print(dec_out.shape)
+        # dec_out = dec_out.reshape(B, self.pred_len + self.seq_len, -1)
+        
+        # De-Normalization from Non-stationary Transformer
+        dec_out = dec_out * \
+                  (stdev[:, 0, :].unsqueeze(1).repeat(
+                      1, self.pred_len + self.seq_len, 1))
+        dec_out = dec_out + \
+                  (means[:, 0, :].unsqueeze(1).repeat(
+                      1, self.pred_len + self.seq_len, 1))
+        
+        return dec_out
+
+
+    def classification(self, x_enc, x_mark_enc):
+        # print(x_enc.shape)
+        B, L, M = x_enc.shape
+        input_x = rearrange(x_enc, 'b l m -> b m l')
+        input_x = self.padding_patch_layer(input_x)
+        input_x = input_x.unfold(dimension=-1, size=self.patch_size, step=self.stride)
+        input_x = rearrange(input_x, 'b m n p -> b n (p m)')
+        
+        outputs = self.enc_embedding(input_x, None)
+        
+        outputs = self.gpt2(inputs_embeds=outputs).last_hidden_state
+
+        outputs = self.act(outputs).reshape(B, -1)
+        outputs = self.ln_proj(outputs)
+        # outputs = self.dropout(outputs)
+        outputs = self.out_layer(outputs)
+        
+        return outputs
